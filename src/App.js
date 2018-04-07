4840d79fc24b3c14ddb39006af2df8af3ac655c8
Drew Higgins <me@drewhiggins.com>
2018-04-07 14:41:27 -0400

    initialized react app with bootstrap

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <h1>Hello, world!</h1>
+      </Container>
+    );
+  }
+}
+
+export default App;
